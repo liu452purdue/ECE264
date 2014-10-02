c0ba87e62c3ccc8ecc2a04020e4a308d43e0f711
Chunan Liu <liu452@ecegrid-thin1.ecn.purdue.edu>
2014-10-02 19:51:20 -0400

    still one more \n?

--- solutions/PA04/cat-lite.c
+++ solutions/PA04/cat-lite.c
@@ -1,61 +1,109 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int int main(int argc, char const *argv[])
+
+#define TRUE 1
+#define FALSE 0
+
+
+int main(int argc, char const *argv[])
 {
     FILE *file;
   
     int i = 0;
+
+    int j = 0;
+
+    int in;
+
     char Content;
     
-    char * help[6];
+    char help[6];
 
     strcpy (help,"--help");
       
-    for (i = 1; i < argc -1; i ++)
+    for (i = 1; i < argc ; i ++)
     {
         if (strcmp(argv[i],help)== 0)
         {
-        fprintf(stdout,"Usage: cat-lite [--help] [FILE]...\n");
-        fprintf(stdout,"With no FILE, or when FILE is -, read standard input.\n");
-        fprintf(stdout,"\n");
-        fprintf(stdout,"Examples:\n");
-        fprintf(stdout,"  cat-lite README   Print the file README to standard output.\n");
-        fprintf(stdout,"  cat-lite f - g    Print f's contents, then standard input,\n");
-        fprintf(stdout,"                    then g's contents.\n");
-        fprintf(stdout,"  cat-lite          Copy standard input to standard output.\n");
-        fprintf(stdout,"\n");
+            fprintf(stdout,"Usage: cat-lite [--help] [FILE]...\n");
+            fprintf(stdout,"With no FILE, or when FILE is -, read standard input.\n");
+            fprintf(stdout,"\n");
+            fprintf(stdout,"Examples:\n");
+            fprintf(stdout,"  cat-lite README   Print the file README to standard output.\n");
+            fprintf(stdout,"  cat-lite f - g    Print f's contents, then standard input,\n");
+            fprintf(stdout,"                    then g's contents.\n");
+            fprintf(stdout,"  cat-lite          Copy standard input to standard output.\n");
+            fprintf(stdout,"\n"); 
         return EXIT_SUCCESS;
         }
     }
 
-    for (i = 0; i < argc -1; i++)
+    
+   if(argc == 1)
+  {
+    do{
+   
+    fputc(fgetc(stdin), stdout);
+      }while (fgetc(stdin) != '\n');
+  }
+
+  else
+  {
+    for (j = 1; j < argc ; ++j)
     {
-       file  = fopen(argv[i],"r");
        
-       if (file == NULL)
+      if (*argv[j] == '-')
        {
-         printf( "cat cannot open %s\n", argv[i] );
-         return EXIT_FAILURE;
-       } 
+         
+         
+         do
+         {
+           
+           fputc(fgetc(stdin), stdout);
+         } while(fgetc(stdin) !='\n');
+
+        }
+       
+       
+        else{
+           file  = fopen(argv[j],"r");
+       
+         if (file == NULL)
+           {
+             printf( "cat cannot open %s\n", argv[j] );
+             return EXIT_FAILURE;
+            } 
 
  
-       else
-       {
-          while (Content != EOF)
-          {
-             Content = fgetc (file);
-              printf("%c", Content);
-          }
-            printf("\n");
-        fclose (pFile);
+          else
+          {  
+       
+            do
+            {
+               Content = fgetc (file);
+                //printf("%c", Content);
 
-       }      
+                 
+      
+            if(Content != EOF)
+              {
+                fputc(Content,stdout);
+              }
+            }while(Content != EOF);
+            
+            //printf("\n");
+          fclose (file);
 
+          }      
+         } 
 
 
     }
     
     return EXIT_SUCCESS;
+  }
 }
+
 
