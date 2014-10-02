5bb9bb526a9039c306b689be7569d4665f602830
Chunan Liu <liu452@ecegrid-thin1.ecn.purdue.edu>
2014-10-02 17:22:05 -0400

    stand input need

--- solutions/PA04/cat-lite.c
+++ solutions/PA04/cat-lite.c
@@ -1,37 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-void _filecopy(FILE *fp_in, FILE * fp_out);
-int main(int argc , char * argv[])
+
+int int main(int argc, char const *argv[])
 {
- 
-    FILE * fp;
-    char *prog = argv[0] ;  //指定的错误输出文件名;
- 
-    if( argc == 1 ) //没有指定打开文件,把标准输入复制到标准输出
-        _filecopy(stdin,stdout);
- 
-    else
-        while(--argc > 0)
-            if((fp = fopen(*++argv,"r")) == NULL) 
-            {//文件打开失败
-                fprintf(stderr,"%s:打开%s失败\n",prog,*argv );
-                exit(1);
-            }else{
-                _filecopy(fp ,stdout);
-                fclose(fp);
-            }
-    if(ferror(stdout)){  //如果输出流出错ferror函数返回一个非零的值
-        fprintf(stderr, "%s:输出流出错了!\n",prog);
-        exit(2);
+    FILE *file;
+  
+    int i = 0;
+    char Content;
+    
+    char * help[6];
+
+    strcpy (help,"--help");
+      
+    for (i = 1; i < argc -1; i ++)
+    {
+        if (strcmp(argv[i],help)== 0)
+        {
+        fprintf(stdout,"Usage: cat-lite [--help] [FILE]...\n");
+        fprintf(stdout,"With no FILE, or when FILE is -, read standard input.\n");
+        fprintf(stdout,"\n");
+        fprintf(stdout,"Examples:\n");
+        fprintf(stdout,"  cat-lite README   Print the file README to standard output.\n");
+        fprintf(stdout,"  cat-lite f - g    Print f's contents, then standard input,\n");
+        fprintf(stdout,"                    then g's contents.\n");
+        fprintf(stdout,"  cat-lite          Copy standard input to standard output.\n");
+        fprintf(stdout,"\n");
+        return EXIT_SUCCESS;
+        }
     }
-    exit(0);
-    return 0;
-}
+
+    for (i = 0; i < argc -1; i++)
+    {
+       file  = fopen(argv[i],"r");
+       
+       if (file == NULL)
+       {
+         printf( "cat cannot open %s\n", argv[i] );
+         return EXIT_FAILURE;
+       } 
+
  
-void _filecopy(FILE *fp_in, FILE * fp_out)
-{
-    int c;
-    while ((c = getc(fp_in)) != EOF)
-        putc(c,fp_out);
+       else
+       {
+          while (Content != EOF)
+          {
+             Content = fgetc (file);
+              printf("%c", Content);
+          }
+            printf("\n");
+        fclose (pFile);
+
+       }      
+
+
+
+    }
+    
+    return EXIT_SUCCESS;
 }
 
