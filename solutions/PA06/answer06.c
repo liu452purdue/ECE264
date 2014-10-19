2254edc4c0d9e15ef75e2c88106ed2235a526280
Chunan Liu <liu452@ecegrid-thin1.ecn.purdue.edu>
2014-10-19 00:37:30 -0400

    debug

--- solutions/PA06/answer06.c
+++ solutions/PA06/answer06.c
@@ -1,15 +1,16 @@
 #include "answer06.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
+#include <string.h>
+
 
 int startpoint(char **maze, int w, int h)
 {
-	int i = 0；
-	char *row1 = maze[0]; // the entry is in the first row 
+	int i = 0;
+	char *firstrow = maze[0]; // the entry is in the first row 
     for (i = 0; i < w; ++i)
     {
-          if (row1[i] == ' ')
+          if (firstrow[i] == ' ')
           {
           	return i;
           }
@@ -17,12 +18,12 @@ int startpoint(char **maze, int w, int h)
     return -1;
 }   
 
-void buffer(char dirction)
+void buffer(char direction)
 {
-	static char dirbuff = 'S'; // record the dirction to go
-	static int dirlen = 0; // record how many steps have been done in this dirction
+	static char dirbuff = 'S'; // record the direction to go
+	static int dirlen = 0; // record how many steps have been done in this direction
 
-	if (dirbuff == dirction)
+	if (dirbuff == direction)
 	{
 
 		dirlen++;
@@ -30,12 +31,12 @@ void buffer(char dirction)
 	else
 	{
 		printf("%c %d\n",dirbuff, dirlen );
-		dirbuff = dirction;
+		dirbuff = direction;
 		dirlen = 1;
 	}
 }
 
-void travelrecursion(char **maze， int w, int h, int x, int y, int prx, int pry)
+void travelrecursion(char **maze, int w, int h, int x, int y, int prx, int pry)
 {
 	/*x = current position in x - axis
 	  y = current position in y - axis
@@ -43,82 +44,57 @@ void travelrecursion(char **maze， int w, int h, int x, int y, int prx, int pry
 	  pry = previous postion in y*/
     int ntx;
     int nty;
-    char dirction;
-}#include "answer06.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/time.h>
-
-int startpoint(char **maze, int w, int h)
-{
-	int i = 0；
-	char *row1 = maze[0]; // the entry is in the first row 
-    for (i = 0; i < w; ++i)
-    {
-          if (row1[i] == ' ')
-          {
-          	return i;
-          }
-    }
-    return -1;
-}   
-
-void buffer(char dirction)
-{
-	static char dirbuff = 'S'; // record the dirction to go
-	static int dirlen = 0; // record how many steps have been done in this dirction
-
-	if (dirbuff == dirction)
-	{
-
-		dirlen++;
-	}
-	else
-	{
-		printf("%c %d\n",dirbuff, dirlen );
-		dirbuff = dirction;
-		dirlen = 1;
-	}
-}
-
-void travelrecursion(char **maze， int w, int h, int x, int y, int prx, int pry)
-{
-	/*x = current position in x - axis
-	  y = current position in y - axis
-	  prx = previous postion in x
-	  pry = previous postion in y*/
-    int ntx;
-    int nty;
-    char dirction;
+    char direction;
     
     direction = 'E';
     ntx = x + 1;
     nty = y;
-    if((w > nx) && (nx >= 0) && (h > ny) && (ny >= 0) &&!((ntx==prx) && (nty==pry)) && (maze[nty][ntx]==' '))		
+    if((w > ntx) && (ntx >= 0) && (h > nty) && (nty >= 0) &&!((ntx==prx) && (nty==pry)) && (maze[nty][ntx]==' '))		
     {
-    	travelrecursion(char **maze， int w, int h, int x, int y, int prx, int pry);
+    	travelrecursion(maze, w, h, x, y, prx, pry);
     }
     
-    dirction = 'W';
+    direction = 'W';
     ntx = x -1;
     nty = y;
- 	if((w > nx) && (nx >= 0) && (h > ny) && (ny >= 0) &&!((ntx==prx) && (nty==pry)) && (maze[nty][ntx]==' '))		
+ 	if((w > ntx) && (ntx >= 0) && (h > nty) && (nty >= 0) &&!((ntx==prx) && (nty==pry)) && (maze[nty][ntx]==' '))		
     {
-    	travelrecursion(char **maze， int w, int h, int x, int y, int prx, int pry);
+
+        travelrecursion(maze, w, h, x, y, prx, pry);
     }
 
-    dirction = 'S';
+    direction = 'S';
     ntx = x;
     nty = y +1;
- 	if((w > nx) && (nx >= 0) && (h > ny) && (ny >= 0) &&!((ntx==prx) && (nty==pry)) && (maze[nty][ntx]==' '))		
+ 	if((w > ntx) && (ntx >= 0) && (h > nty) && (nty >= 0) &&!((ntx==prx) && (nty==pry)) && (maze[nty][ntx]==' '))		
     {
-    	travelrecursion(char **maze， int w, int h, int x, int y, int prx, int pry);
+    
+        travelrecursion(maze, w, h, x, y, prx, pry);
     }
 
-    dirction = 'N';
+    direction = 'N';
     ntx = x;
     nty = y-1;
- 	if((w > nx) && (nx >= 0) && (h > ny) && (ny >= 0) &&!((ntx==prx) && (nty==pry)) && (maze[nty][ntx]==' '))		
+ 	if((w > ntx) && (ntx >= 0) && (h > nty) && (nty >= 0) &&!((ntx==prx) && (nty==pry)) && (maze[nty][ntx]==' '))		
     {
-    	travelrecursion(char **maze， int w, int h, int x, int y, int prx, int pry);
+        travelrecursion(maze, w, h, x, y, prx, pry);
     }
+  }  
+
+void print_directions(char **maze, int w, int h)
+{
+	int start;
+	start = startpoint(maze,w,h);
+	if(start==-1)
+	{
+		printf("no start found for first line\n%s\n" , maze[0]);
+		return;
+	}
+	else
+	{
+		recursiveTravel(maze , w , h , start , 0 , -1 , -1);
+		buffer(' ');
+ 	}
+
+}
+
